5374b38104f30a788b039b1e73e415eeb37abb3f
DuMark <dumark.allman@yahoo.com>
2024-01-11 22:48:32 -0500

    Coming along nicely!

--- index.js
+++ index.js
@@ -13,4 +13,20 @@ renderRecipes()
  const ingredientsContainer = document.getElementById("ingredients-container");
  const instructionsContainer  = document.getElementById("instructions-container");
 
- 
+ recipeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("Submitted Successfully!");
+
+    let newRecipe = {
+        name:e.target.recipeName.value,
+        image:e.target.recipeImage.value,
+        ingredients: e.target.recipeIngredients.value,
+        instructionsContainer: e.target.recipeInstructions.value
+    };
+
+    createNewRecipe(newRecipe);
+    recipeForm.reset();
+});
+
+function showRecipes(recipe) {
+    
