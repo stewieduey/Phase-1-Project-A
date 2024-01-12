c257816bac33df6375f83775889fc8f2618c96b5
DuMark <dumark.allman@yahoo.com>
2024-01-11 22:41:29 -0500

    Coming along nicely!

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => fetchData);
+document.addEventListener("DOMContentLoaded", () => 
+renderRecipes()
+);
 
  function fetchData(){
     fetch('https://localhost:3000/recipes')
     .then((resp) => resp.json())
     .then((data) => console.log(data))
  };
+
+ const recipeForm  = document.getElementById("recipeForm");
+ const recipeContainer = document.getElementById("recipe-container");
+ const ingredientsContainer = document.getElementById("ingredients-container");
+ const instructionsContainer  = document.getElementById("instructions-container");
+
+ 
