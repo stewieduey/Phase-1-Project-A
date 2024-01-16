dbb7493c63af14499b604ae84ae284331df70a82
DuMark <dumark.allman@yahoo.com>
2024-01-16 14:20:52 -0500

    Coming along nicely!

--- index.js
+++ index.js
@@ -28,63 +28,73 @@ renderRecipes()
     recipeForm.reset();
 });
 
+
 function showRecipes(recipe) {
     recipeContainer.replaceChildren();
-    console.log("recipe clicked!");
+    ingredientsContainer.replaceChildren();
+    instructionsContainer.replaceChildren();
+  
     let recipeDescription = document.createElement("p");
     recipeDescription.classList = "recipe-description";
     recipeDescription.textContent = recipe.name;
     console.log("Recipe desc:", recipeDescription);
     console.log(recipeContainer);
     recipeContainer.appendChild(recipeDescription);
-    let ingredientsList = document.createElement("ul");
-  recipe.ingredients.forEach(ingredient => {
-    let ingredientItem = document.createElement("li");
-    ingredientItem.textContent = ingredient;
-    ingredientsList.appendChild(ingredientItem);
-  });
-
-  ingredientsContainer.appendChild(ingredientsList);
-}
   
-function renderRecipes() {
+    if (Array.isArray(recipe.ingredients)) {
+      let ingredientsList = document.createElement("ul");
+      recipe.ingredients.forEach(ingredient => {
+        let ingredientItem = document.createElement("li");
+        ingredientItem.textContent = ingredient;
+        ingredientsList.appendChild(ingredientItem);
+      });
+  
+      ingredientsContainer.appendChild(ingredientsList);
+    } else {
+      let errorText = document.createElement("p");
+      errorText.textContent = "Ingredients not available";
+      ingredientsContainer.appendChild(errorText);
+    }
+  
+    let instructionsText = document.createElement("p");
+    instructionsText.textContent = recipe.instructions;
+    instructionsContainer.appendChild(instructionsText);
+  }
+  
+  function renderRecipes() {
     recipeContainer.replaceChildren();
     fetch("http://localhost:3000/recipes", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(),
-      })
-        .then((resp) => resp.json())
-        .then((recipes) => {
-          recipes.map((recipe) => {
-            const card = document.createElement("div");
-            card.classList = "recipe-card";
-            const img = document.createElement("img");
-            img.src = recipe.image;
-            const name = document.createElement("p");
-            name.textContent = recipe.name;
-    
-            card.appendChild(img);
-            card.appendChild(name);
-    
-            card.addEventListener("click", () => {
-              showRecipes(recipe);
-              console.log(recipe);
-            });
-    
-            recipeContainer.append(card);
-            // return card;
-          });
-             //   recipeContainer.append(...heroElements);
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
     })
-    .catch((error) => {
-      console.error("Hello from the othersiiiiiiide!!!", error.message);
-      //added this to let me know if something is wrong in a fun way.
-    });
-}
-
+      .then((resp) => resp.json())
+      .then((recipes) => {
+        recipes.forEach((recipe) => {
+          const card = document.createElement("div");
+          card.classList = "recipe-card";
+          const img = document.createElement("img");
+          img.src = recipe.image;
+          const name = document.createElement("p");
+          name.textContent = recipe.name;
+  
+          card.appendChild(img);
+          card.appendChild(name);
+  
+          card.addEventListener("click", () => {
+            showRecipes(recipe);
+            console.log(recipe);
+          });
+  
+          recipeContainer.appendChild(card);
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+    }
   //The display my faves toggle button event listener
   const showFavesButton = document.getElementById("showFavesButton");
   showFavesButton.addEventListener("click", toggleFavoriteRecipes);
